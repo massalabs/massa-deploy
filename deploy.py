040a632dd431619c790704b84e4eaf941e328258
Yvan Sraka <yvan@sraka.xyz>
2021-12-08 18:53:33 +0100

    Make the deployment script simpler ✨
    It deploy the massa project in which you are whatever the version
    checkout for more simplicity/flexibility and future integration to
    the CI! 🌈

--- deploy.py
+++ deploy.py
@@ -1,34 +1,22 @@
-import subprocess
-import toml
 import copy
 import json
 import os
 import sys
+import toml
+
+ROOT_PATH = ".."  # this script is run inside a subfolder of massa git repository
+CONFIG_PATH = f'{ROOT_PATH}/massa-node/base_config/config.toml'
+PEERS_PATH = f'{ROOT_PATH}/massa-node/storage/peers.json'
+WALLET_PATH = f'{ROOT_PATH}/massa-client/wallet.dat'
+STAKING_KEYS_PATH = f'{ROOT_PATH}/massa-node/config/staking_keys.json'
+NODE_PRIVKEY_PATH = f'{ROOT_PATH}/massa-node/config/node_privkey.key'
 
 
-def run_cmd(cmd, ignore_err=False):
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    process.communicate()
-    if process.returncode != 0 and ignore_err == False:
-        raise Exception("process failed")
-
-
-def deploy(tag, servers):
-    # git clone
-    print("cloning git...")
-    run_cmd("rm -rf massa")
-    run_cmd("git clone --depth 1 --branch " + tag +
-            " https://github.com/massalabs/massa")
-
+if __name__ == "__main__":
     # load config files
     print("loading config file...")
-    config_path = "massa/massa-node/base_config/config.toml"
-    peers_path = "massa/massa-node/storage/peers.json"
-    wallet_path = "massa/massa-client/wallet.dat"
-    staking_keys_path = "massa/massa-node/config/staking_keys.json"
-    node_privkey_path = "massa/massa-node/config/node_privkey.key"
-    config_data = toml.load(config_path)
-    with open(peers_path) as json_file:
+    config_data = toml.load(CONFIG_PATH)
+    with open(PEERS_PATH) as json_file:
         peers = json.load(json_file)
 
     testnet_user = os.environ['TESTNET_USER']
@@ -73,37 +61,30 @@ def deploy(tag, servers):
     # }
     srvs = os.environ['TESTNET_SRVS']
 
-    print("preparing launch script...")
-    with open("massa/massa-node/launch.sh", "w") as outf:
-        outf.write("nohup cargo run --release > logs.txt 2>&1 &\n")
-
     for srv_name, srv in srvs.items():
-        if srv_name not in servers:
-            continue
         print("distributing to", srv_name, "...")
 
         # setup node staking keys
         res_staking_keys = [srv["staking_privkey"], ]
-        with open(staking_keys_path, "w") as json_file:
+        with open(STAKING_KEYS_PATH, "w") as json_file:
             json_file.write(json.dumps(res_staking_keys, indent=2))
 
         # setup peers
         res_peers = [p for p in peers if p["ip"] != srv["ip"]]
-        with open(peers_path, "w") as json_file:
+        with open(PEERS_PATH, "w") as json_file:
             json_file.write(json.dumps(res_peers, indent=2))
 
         # setup node privkey
-        with open(node_privkey_path, "w") as outf:
+        with open(NODE_PRIVKEY_PATH, "w") as outf:
             outf.write(srv["node_privkey"])
 
         # setup node wallet
         res_wallet = [srv["staking_privkey"]]
-        with open(wallet_path, "w") as json_file:
+        with open(WALLET_PATH, "w") as json_file:
             json_file.write(json.dumps(res_wallet, indent=2))
 
         # setup config
         cfg = copy.deepcopy(config_data)
-        cfg["version"] = tag
         cfg["network"]["target_bootstrap_connections"] = len(
             srvs) - 1  # connect to all bootstrap srvs
         # connect to 3 peers
@@ -120,24 +101,16 @@ def deploy(tag, servers):
         if srv["bootstrap_server"] is False:
             del cfg["bootstrap"]["bind"]
 
-        with open(config_path, "w") as toml_file:
+        with open(CONFIG_PATH, "w") as toml_file:
             toml.dump(cfg, toml_file)
 
         # upload to server
-        run_cmd("zip -r massa.zip massa")
-        run_cmd('sshpass -p "' + testnet_pwd + '" ssh testnet@' +
-                srv["ip"] + ' "pkill -f massa-node"', ignore_err=True)
-        run_cmd('sshpass -p "' + testnet_pwd + '" ssh testnet@' +
-                srv["ip"] + ' "rm -rf massa"', ignore_err=True)
-        run_cmd('sshpass -p "' + testnet_pwd + '" scp -r -o StrictHostKeyChecking=no massa.zip testnet@' +
-                srv["ip"] + ':/home/testnet/massa.zip')
-        run_cmd('sshpass -p "' + testnet_pwd + '" ssh testnet@' +
-                srv["ip"] + ' "source /home/testnet/.cargo/env && unzip massa.zip && rm massa.zip && cd massa/massa-node && chmod +x launch.sh && ./launch.sh"')
-        run_cmd("rm massa.zip")
+        host = f'{testnet_user}@{srv["ip"]}'
+        os.system(f'rsync -azP --delete {ROOT_PATH} {host}:~')
 
-    run_cmd("rm -rf massa")
-    print("done")
+        from fabric import Connection
+        c = Connection(host=host, connect_kwargs={"password": testnet_pwd})
+        c.run('pkill -f massa-node')
+        c.run('source ~/.cargo/env && cd ~/massa/massa-node && nohup cargo run --release > logs.txt 2>&1 &')
 
-
-if __name__ == "__main__":
-    deploy(sys.argv[1], sys.argv[2:])
+    print("done 🎉")
